ec03113ec6d4ac4845c058d0e71638577aa85abb
Hong Jeon <hongj77@gmail.com>
2017-05-10 13:08:03 -0400

    fix replay memory

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # General
 STEPS_PER_EPOCH = 10000 # temp
 RUN_TILL_EPOCH = 2000 # how many to run
-SESSION_NAME = "hong_mac2"
+SESSION_NAME = "hong_gpu2"
 
 # AI QTable
 ai_qtable_learning_rate = .85
@@ -15,7 +15,7 @@ ai_final_epsilon = .1 # google
 ai_epsilon_anneal_rate = 1.0/1000000 # google
 ai_replay_mem_start_size = 50000 # temp
 # ai_replay_mem_total_size = 1000000 # this results in memory running out around 400k-600k
-ai_replay_mem_total_size = 100000 # maybe try 100k instead
+ai_replay_mem_total_size = 300000 # maybe try 100k instead
 
 # Network Save
 net_should_save = True
@@ -31,7 +31,7 @@ net_height = 84
 net_width = 84
 net_num_screens = 4
 net_n_actions = 6
-net_discount_factor = .97 # google
+net_discount_factor = .99 # google
 net_dropout = 1 # probability to keep units. Not using this right now
 net_train_rate = 4 # how many steps for 1 train
 
